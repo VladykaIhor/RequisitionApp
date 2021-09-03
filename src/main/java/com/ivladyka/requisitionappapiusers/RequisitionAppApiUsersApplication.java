190cd22a0c8043551b85253f415ff89eb5555b7a
ivladyka <ivladyka@github.com>
2021-09-03 21:01:52 +0300

    added password encryption

--- src/main/java/com/ivladyka/requisitionappapiusers/RequisitionAppApiUsersApplication.java
+++ src/main/java/com/ivladyka/requisitionappapiusers/RequisitionAppApiUsersApplication.java
@@ -1,5 +1,7 @@
 package com.ivladyka.requisitionappapiusers;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
+import com.ulisesbocchio.jasyptspringboot.annotation.EncryptablePropertySource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
